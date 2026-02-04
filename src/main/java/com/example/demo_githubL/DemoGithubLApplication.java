f7fc613cf1c4e4ab1aef66d589b016d991830351
BENHAMOUCHE Lehna OBS/OBF <KCZP3947@orange.com>
2026-02-04 09:56:43 +0100

    Initial commit du projet Spring Boot Maven

---
+++ src/main/java/com/example/demo_githubL/DemoGithubLApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_githubL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoGithubLApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoGithubLApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_githubL/DemoGithubLApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_githubL;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoGithubLApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
