835c32869772c66f5f6fc56dffaf60277e903b45
lhnEf <lehna.benhamouche@efrei.net>
2026-02-04 12:25:31 +0100

    Update DemoGithubLApplication.java

--- src/main/java/com/example/demo_githubL/DemoGithubLApplication.java
+++ src/main/java/com/example/demo_githubL/DemoGithubLApplication.java
@@ -1,13 +1,23 @@
-package com.example.demo_githubL;
-
+package com.example.demo;
+ 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+ 
 @SpringBootApplication
-public class DemoGithubLApplication {
-
+public class DemoApplication {
+ 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoGithubLApplication.class, args);
+		SpringApplication.run(DemoApplication.class, args);
 	}
-
+ 
+}
+ 
+@RestController
+class HelloWorldController {
+	@GetMapping("/")
+	public String sayHello() {
+		return "Hello, World Lehna";
+	}
 }
