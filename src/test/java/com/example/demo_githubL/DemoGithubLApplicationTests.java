ee23685edd258f4d3a222a92dd84df7a845b6668
lhnEf <lehna.benhamouche@efrei.net>
2026-02-04 12:31:36 +0100

    Update DemoGithubLApplicationTests.java

--- src/test/java/com/example/demo_githubL/DemoGithubLApplicationTests.java
+++ src/test/java/com/example/demo_githubL/DemoGithubLApplicationTests.java
@@ -2,6 +2,8 @@ package com.example.demo_githubL;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class DemoGithubLApplicationTests {
@@ -11,3 +13,18 @@ class DemoGithubLApplicationTests {
 	}
 
 }
+
+@RestController
+
+class HelloWorldController {
+
+	@GetMapping("/")
+
+	public String sayHello() {
+
+		return "Hello, World Lehna";
+
+	}
+
+}
+ 
